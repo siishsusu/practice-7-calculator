1a0b0bbc61bc00f9774fcd63d4398f27ac5e7d50
siishsusu <byvladyslava@gmail.com>
2024-11-18 18:20:19 +0200

    finished

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,32 @@ use pest::Parser;
 #[derive(Debug)]
 pub enum Expr {
     Integer(i32),
+    UnaryMinus(Box<Expr>),
     BinOp {
         lhs: Box<Expr>,
         op: Op,
         rhs: Box<Expr>,
     },
+}
+
+impl Expr {
+    pub fn evaluate(&self) -> i32 {
+        match self {
+            Expr::Integer(value) => *value,
+            Expr::UnaryMinus(expr) => -expr.evaluate(),
+            Expr::BinOp { lhs, op, rhs } => {
+                let left = lhs.evaluate();
+                let right = rhs.evaluate();
+                match op {
+                    Op::Add => left + right,
+                    Op::Subtract => left - right,
+                    Op::Multiply => left * right,
+                    Op::Divide => left / right,
+                    Op::Modulo => left % right,
+                }
+            }
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -21,6 +42,7 @@ pub enum Op {
     Subtract,
     Multiply,
     Divide,
+    Modulo,
 }
 
 #[derive(Parser)]
@@ -34,9 +56,9 @@ lazy_static::lazy_static! {
 
         // Пріоритет визначається від найнижчого до найвищого
         PrattParser::new()
-            // Додавання та віднімання мають однаковий пріоритет
             .op(Op::infix(add, Left) | Op::infix(subtract, Left))
-            .op(Op::infix(multiply, Left) | Op::infix(divide, Left))
+            .op(Op::infix(multiply, Left) | Op::infix(divide, Left) | Op::infix(modulo, Left))
+            .op(Op::prefix(unary_minus))
     };
 }
 
@@ -44,7 +66,12 @@ pub fn parse_expr(pairs: Pairs<Rule>) -> Expr {
     PRATT_PARSER
         .map_primary(|primary| match primary.as_rule() {
             Rule::integer => Expr::Integer(primary.as_str().parse::<i32>().unwrap()),
-            rule => unreachable!("Expr::parse expected atom, found {:?}", rule)
+            Rule::expr => parse_expr(primary.into_inner()),
+            rule => unreachable!("Expr::parse expected atom, found {:?}", rule),
+        })
+        .map_prefix(|op, rhs| match op.as_rule() {
+            Rule::unary_minus => Expr::UnaryMinus(Box::new(rhs)),
+            _ => unreachable!(),
         })
         .map_infix(|lhs, op, rhs| {
             let op = match op.as_rule() {
@@ -52,6 +79,7 @@ pub fn parse_expr(pairs: Pairs<Rule>) -> Expr {
                 Rule::subtract => Op::Subtract,
                 Rule::multiply => Op::Multiply,
                 Rule::divide => Op::Divide,
+                Rule::modulo => Op::Modulo,
                 rule => unreachable!("Expr::parse expected infix operation, found {:?}", rule),
             };
             Expr::BinOp {
@@ -68,12 +96,8 @@ fn main() -> io::Result<()> {
     for line in io::stdin().lock().lines() {
         match CalculatorParser::parse(Rule::equation, &line?) {
             Ok(mut pairs) => {
-                println!(
-                    "Parsed: {:#?}",
-                    parse_expr(
-                        pairs.next().unwrap().into_inner()
-                    )
-                );
+                let expr = parse_expr(pairs.next().unwrap().into_inner());
+                println!("Result: {}", expr.evaluate());
             }
             Err(e) => {
                 eprintln!("Parse failed: {:?}", e);
