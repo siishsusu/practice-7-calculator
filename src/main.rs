aa5149cbd453067d5bd746eb59dfcd678b45e1be
siishsusu <byvladyslava@gmail.com>
2024-11-18 17:53:06 +0200

    added ast

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+#[derive(Debug)]
+pub enum Expr {
+    Integer(i32),
+    BinOp {
+        lhs: Box<Expr>,
+        op: Op,
+        rhs: Box<Expr>,
+    },
+}
+
+#[derive(Debug)]
+pub enum Op {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+}
+
 fn main() {
     println!("Hello, world!");
 }
